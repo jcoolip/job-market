cd244e1e19ecf58e6f039bd0ebc7aa042c937a1e
Justin Cullop <jcoolip@gmail.com>
2026-03-07 22:14:09 -0500

    inserting log files

--- ingest/pipeline.py
+++ ingest/pipeline.py
@@ -4,20 +4,41 @@ import usajobs
 import usajobs_skills
 import adzuna
 import adzuna_skills
+import logging
+from datetime import datetime
+
+
+now = datetime.now()
+
+basicConfig(
+    level=logging.DEBUG,
+    filename=f"pipeline{now}.log",
+    filemode="w",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
 
 def main():
-    print("_.-`` remotive ``-._")
+    logging.info("attempt remotive jobs")
+    # print("_.-`` remotive ``-._")
     remotive.main()
-    print("``-._  skills  _.-``")
+    logging.info("attempt remotive skills")
+    # print("``-._  skills  _.-``")
     remotive_skills.main()
-    print("_.-`` usajobs ``-._")
+    logging.info("attempt usajobs jobs")
+    # print("_.-`` usajobs ``-._")
     usajobs.main()
-    print("``-._  skills  _.-``")
+    logging.info("attempt usajobs skills")
+    # print("``-._  skills  _.-``")
     usajobs_skills.main()
-    print("_.-``  adzuna  ``-._")
+    logging.info("attempt adzuna")
+    # print("_.-``  adzuna  ``-._")
     adzuna.main()
-    print("``-._  skills  _.-``")
+    logging.info("attempt adzuna skills")
+    # print("``-._  skills  _.-``")
     adzuna_skills.main()
+
 
 if __name__ == "__main__":
     main()
+
